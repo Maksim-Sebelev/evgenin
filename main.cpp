07465b4ef10e2e25741701539b00fbafdb73fab8
Maksim-Sebelev <7905111@list.ru>
2024-11-12 21:06:10 +0300

    че

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "onegin.h"
+
+int main()
+{
+    const char* file1 = "test1.txt";
+    
+    size_t buf1Size = 0;
+    char** buffer1 = ReadBufferFromFile(file1, &buf1Size);
+
+    for (size_t i = 0; i < buf1Size; i++)
+    {
+        printf("buf1[%2lu]: '%s'\n", i, buffer1[i]);
+    }
+    BufferDtor(buffer1);
+
+
+    //-----------------------------------------------------
+    printf("\n\n");
+    //-----------------------------------------------------
+
+
+    const char* file2 = "test2.txt";
+
+    size_t buf2Size = 0;
+    char** buffer2 = ReadBufferFromFile(file2, &buf2Size);
+
+    for (size_t i = 0; i < buf2Size; i++)
+    {
+        printf("buf2[%2lu]: '%d'\n", i, strtoint(buffer2[i]));
+    }
+    BufferDtor(buffer2);
+
+    return 0;
+}
---
+++ onegin.cpp
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <sys/stat.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include "onegin.h"
+
+static char   GetArrElem    (char* arr, size_t elemIndex);
+static char*  GetArrElemPtr (char* arr, size_t elemIndex);
+static size_t CalcFileLen   (const char* FileName);
+static void   SetLine       (char** split_buffer, size_t* word_i, char* setLine);
+static bool   IsPassSymbol  (char c);
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+int strtoint(char* str)
+{
+    long res = 0;
+    char* strEnd = nullptr;
+    res = strtol(str, &strEnd, 10);
+
+    assert(strlen(str) == (size_t) (strEnd - str)); // проверка на то, вся ли строка является числом
+
+    return (int) res;
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+char** ReadBufferFromFile(const char* file, size_t* bufSize)
+{
+    FILE* filePtr = fopen(file, "rb");
+    assert(filePtr);
+
+    size_t bufferLen = CalcFileLen(file);
+
+    char*  buffer       = (char*)   calloc(bufferLen + 1, sizeof(char));
+    char** split_buffer = (char**)  calloc(bufferLen + 1, sizeof(char*));
+
+    fread(buffer, sizeof(char), bufferLen + 1, filePtr);
+    buffer[bufferLen] = '\0';
+
+    fclose(filePtr);
+
+    char bufElem = GetArrElem(buffer, 0);
+
+    assert(bufElem != ' ');
+    assert(bufElem != '\n');
+
+    bufElem = GetArrElem(buffer, bufferLen - 1);
+
+    assert(bufElem == '\n');
+
+    size_t word_i = 0;
+
+    SetLine(split_buffer, &word_i, GetArrElemPtr(buffer, 0));
+
+    for (size_t buffer_i = 0; buffer_i <= bufferLen; buffer_i++)
+    {
+        bufElem = GetArrElem(buffer, buffer_i);
+        if (IsPassSymbol(bufElem))
+        {
+            while (IsPassSymbol(bufElem) && buffer_i <= bufferLen)
+            {
+                buffer[buffer_i] = '\0';
+                buffer_i++;
+                bufElem = GetArrElem(buffer, buffer_i);
+            }
+    
+            SetLine(split_buffer, &word_i, GetArrElemPtr(buffer, buffer_i));
+        }
+    }
+
+    *bufSize = word_i - 1;
+
+    split_buffer = (char**) realloc(split_buffer, (*bufSize) * sizeof(char*));
+
+    assert(split_buffer);
+    assert(*split_buffer);
+
+    assert(split_buffer);
+    return split_buffer;
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+void BufferDtor(char** buffer)
+{
+    assert(buffer);
+
+    FREE(*buffer)
+    FREE(buffer);
+
+    assert(!buffer);
+
+    return;
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+static size_t CalcFileLen(const char* FileName)
+{
+    struct stat buf = {};
+    stat(FileName, &buf);
+    return (size_t) buf.st_size;
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+static char GetArrElem(char* arr, size_t elemIndex)
+{
+    assert(arr);
+    return arr[elemIndex];
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+static char* GetArrElemPtr(char* arr, size_t elemIndex)
+{
+    assert(arr);
+    return &arr[elemIndex];
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+static void SetLine(char** split_buffer, size_t* word_i, char* setLine)
+{
+    assert(split_buffer);
+    assert(setLine);
+    assert(word_i);
+
+    split_buffer[*word_i] = setLine;
+    (*word_i)++;
+    return;
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+static bool IsPassSymbol(char c)
+{
+    return (c == ' ') || (c == '\n');
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
